cf8b58907134e8b3a6930fcf97becb530a5135e0
Haider Nadeem <haider.mzr@gmail.com>
2021-07-05 01:51:28 +0500

    added application colours

--- themes/Colors.js
+++ themes/Colors.js
@@ -1,5 +1,7 @@
-export const Orange = "##FFAB70";
+export const Orange = "#FFAB70";
 export const Blue = "#89CFF0";
 export const Green = "#BAE67E";
 export const White = "#ecebe6";
 export const Purple = "#e2c5ff";
+export const Yellow = "#FFD580";
+export const BackgroundGrey = "#282C34";
